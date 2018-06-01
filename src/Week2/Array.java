f2cb187a1d30af4fdfe65eff84887292eafea5c7
Inés Van Gucht <inesvangucht@gmail.com>
2018-06-01 09:24:29 +0200

    einde dag

---
+++ src/Week2/Array.java
@@ -0,0 +1,15 @@
+package Week2;
+
+public class Array {
+    public static void main(String[] args) {
+
+        int i = 5; // "scalar" "variable"
+
+        int[] j = {1, 2 , 5, 7, 9, -6, 12};
+        System.out.println(j.length); //weergeven lengte van de array
+
+        int getal = j[1];
+        System.out.println(getal); // input opvragen op basis van indexpositie
+
+    }
+}
---
+++ src/Week2/ArrayOefening1.java
@@ -0,0 +1,18 @@
+package Week2;
+
+public class ArrayOefening1 {
+    public static void main(String[] args) {
+
+
+        int[] j = {1, 2 , 5, 7, 9, -6, 12};
+
+
+        for (int teller = 0; teller < j.length; teller++) {
+
+            int getal = j[teller];
+            System.out.println(getal);
+
+        }
+    }
+
+}
