2efd7ab8338970248e0846896359f75b73446d34
Inés Van Gucht <inesvangucht@gmail.com>
2018-06-04 15:53:09 +0200

    4 juni avond

---
+++ src/be/vdab/beehive/Soldier.java
@@ -0,0 +1,30 @@
+package be.vdab.beehive;
+
+public class Soldier {
+
+    int hitpoints = 100;
+    int strength = 10;
+    int armour = 5;
+    int attack = 10;
+    int critical;
+
+
+
+    public void strongAttack(Soldier target) {
+
+       target.hitpoints -= (this.strength * 1.5) - target.armour;
+       this.hitpoints -= (target.strength * 0.5) - this.armour;
+
+    }
+
+    public void quickAttack() {
+
+    }
+
+    public boolean isDead() {
+        return hitpoints <= 0;
+    }
+
+
+
+}
--- src/be/vdab/beehive/Worker.java
+++ src/be/vdab/beehive/Worker.java
@@ -1,4 +1,26 @@
 package be.vdab.beehive;
 
 public class Worker {
+
+    //buiten de static, hier bepalen workers hebben een capacity en een load
+
+    int capacity;
+    int load;
+
+    public Worker (int initialCapacity, int initialLoad) {
+        System.out.println("Constructor");
+        capacity = initialCapacity;
+        load = initialLoad;
+    }
+
+    //niet static, static is een globale functie. steunt niet op context
+
+    public void gatherNectar() {
+        for (int i=0; i <10 && capacity > load; i++) {
+            //visit flower
+            System.out.println("Visit next flower.");
+            load +=5;
+        }
+    }
+
 }
